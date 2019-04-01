56e5764571369a8f200f24bdbbb16b3545fe1dd2
Aryan Jain <aryanjn807@gmail.com>
2019-04-02 03:20:05 +0530

    Added Assignment-4

--- SE-2/Computer Graphics Laboratory/4.Draw_Pattern/Assignment_4.cpp
+++ SE-2/Computer Graphics Laboratory/4.Draw_Pattern/Assignment_4.cpp
@@ -1,102 +1,90 @@
-/*Draw the following pattern using any Line drawing algorithms.*/
-
-#include<iostream>
-#include<time.h>
 #include<GL/gl.h>
 #include<GL/glu.h>
 #include<GL/glut.h>
+#include<iostream>
 
 using namespace std;
 
-void dis();
-void dda(int x1,int y1, int x2, int y2);
-void init();
+void Display();
+void Reshape();
+void dda(int x1, int y1, int x2, int y2);
 
 int x1,x2,y1,y2;
-int w;
 
 int main(int a, char **c)
 {
-	cout << "Enter co-ordinates below : \n\n";
-	cout << "Enter X1 & Y1 : ";
+	cout << "Enter (X1,Y1) : ";
 	cin >> x1 >> y1;
-	
-	cout << "Enter X2 & Y2 : ";
+	cout << "Enter (X2,Y2) : ";
 	cin >> x2 >> y2;
 
-	cout << "Enter width of line (in pixel) : ";
-	cin >> w;
-	
 	glutInit(&a, c);
-	glutInitWindowSize(800, 800);
-	glutInitWindowPosition(500, 500);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(200, 200);
 	glutInitDisplayMode(GLUT_RGB);
 
-	glutCreateWindow("CG :: Assignment_4.");
-	init();
-	glutDisplayFunc(dis);
+	glutCreateWindow("Assignment--1");
+	Reshape();
+	glutDisplayFunc(Display);	
 	glutMainLoop();
 }
 
-void init()
+void Reshape()
 {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(-1000, 1000, -1000, 1000);
-
 	glMatrixMode(GL_MODELVIEW);
 }
 
-void dis()
-{	
+void Display()
+{
 	glClear(GL_COLOR_BUFFER_BIT);
-	glClearColor(0,0,0,1);
-	
-	dda(x1,y1,x2,y1);
-	dda(x1,y1,x1,y2);
-	dda(x1,y2,x2,y2);
-	dda(x2,y1,x2,y2);
+	glClearColor(0.0,0.0,0.0,1.0);
 
-	dda((x1+x2)/2,y1,x2,(y1+y2)/2);
+	dda(x1, y1, x2, y1);
+	dda(x1, y1, x1, y2);
+	dda(x2, y2, x1, y2);
+	dda(x2, y2, x2, y1);
+
+	dda((x1+x2)/2,y1,x2,(y1+y2)/2);	
 	dda((x1+x2)/2,y1,x1,(y1+y2)/2);
-	dda(x1,(y1+y2)/2,(x1+x2)/2,y2);
-	dda(x2,(y1+y2)/2,(x1+x2)/2,y2);
+	dda((x1+x2)/2,y2,x1,(y1+y2)/2);
+	dda((x1+x2)/2,y2,x2,(y1+y2)/2);
 
-	dda((((x1+x2)/2)+x1)/2,(((y1+y2)/2)+y1)/2,(((x1+x2)/2)+x2)/2, (((y1+y2)/2)+y1)/2);
-	dda((((x1+x2)/2)+x1)/2,(((y1+y2)/2)+y1)/2,(((x1+x2)/2)+x1)/2, (((y1+y2)/2)+y2)/2);
-	dda((((x1+x2)/2)+x1)/2, (((y1+y2)/2)+y2)/2,(((x1+x2)/2)+x2)/2,(((y1+y2)/2)+y2)/2);
-	dda((((x1+x2)/2)+x2)/2, (((y1+y2)/2)+y1)/2,(((x1+x2)/2)+x2)/2,(((y1+y2)/2)+y2)/2);
-	
+	dda((x1+(x1+x2)/2)/2,(y1+(y1+y2)/2)/2,(x2+(x1+x2)/2)/2,(y1+(y1+y2)/2)/2);
+	dda((x1+(x1+x2)/2)/2,(y1+(y1+y2)/2)/2,(x1+(x1+x2)/2)/2,(y2+(y1+y2)/2)/2);
+	dda((x2+(x1+x2)/2)/2,(y2+(y1+y2)/2)/2,(x1+(x1+x2)/2)/2,(y2+(y1+y2)/2)/2);
+	dda((x2+(x1+x2)/2)/2,(y2+(y1+y2)/2)/2,(x2+(x1+x2)/2)/2,(y1+(y1+y2)/2)/2);
+
 	glFlush();
 }
-void dda(int x1,int y1, int x2, int y2)
-{	
-	int steps;
+
+void dda(int x1, int y1, int x2, int y2)
+{
+	float dx, dy;
+	int m, steps;
 	int xinc, yinc;
-	int dx, dy;
 
-	dx = (x2-x1);
-	dy = (y2-y1);
+	dx = abs(x2-x1);
+	dy = abs(y2-y1);
 
-	if(dx>=dy)
-	{
+	m = dy/dx;
+
+	if(dx >= dy)
 		steps = dx;
-	}
-	else if(dx<dy)
-	{
+	else
 		steps = dy;
-	}
-
-	int x = x1;
-	int y = y1; 	
 	
-	xinc = dx/steps;
-	yinc = dy/steps;
+	int x = x1;
+	int y = y1;
+
+	xinc = (x2-x1)/steps;
+	yinc = (y2-y1)/steps;
 
 	for(int i=1; i<=steps; i++)
 	{
-		
-		glPointSize(w);
+		glPointSize(2);
 		glBegin(GL_POINTS);
 		glVertex2i(x,y);
 		glEnd();
