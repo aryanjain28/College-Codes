aa7849235f36a91fa2c914074cfab4b062a0992c
Aryan Jain <aryanjn807@gmail.com>
2019-11-04 02:07:45 +0530

    Add files via upload

---
+++ TE-1/OS/Assignment_8/Server.c
@@ -0,0 +1,18 @@
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
+#include <stdio.h> 
+
+int main(){
+ 
+	key_t key = 5678; 
+ 
+	int shmid = shmget(key,1024,0666|IPC_CREAT); 
+
+	char *str = (char*) shmat(shmid,(void*)0,0); 
+
+	printf("Write Data : ");
+
+	scanf("%s", str);
+
+	printf("Data written in memory: %s\n",str); 
+} 
